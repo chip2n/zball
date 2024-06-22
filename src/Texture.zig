e139c0e14c5ddced12cc4a0a7ddc40007d095ce5
chip2n <andreas@arvidsson.io>
2024-06-22 14:52:46 +0200

    Load texture using stb_image.h

--- src/Texture.zig
+++ src/Texture.zig
@@ -13,18 +13,13 @@ pub fn init() @This() {
     var x: c_int = undefined;
     var y: c_int = undefined;
     var channels: c_int = undefined;
-    _ = c.stbi_load_from_memory(img, img.len, &x, &y, &channels, 4);
-    std.log.warn("{} {} {}", .{x, y, channels});
+    const img_data = c.stbi_load_from_memory(img, img.len, &x, &y, &channels, 4);
+    const size: usize = @intCast(x * y * 4);
     var desc: sg.ImageDesc = .{
-        .width = 4,
-        .height = 4,
+        .width = x,
+        .height = y,
         .pixel_format = .RGBA8,
     };
-    desc.data.subimage[0][0] = sg.asRange(&[4 * 4]u32{
-        0xFFFFFFFF, 0xFF000000, 0xFFFFFFFF, 0xFF000000,
-        0xFF000000, 0xFFFFFFFF, 0xFF000000, 0xFFFFFFFF,
-        0xFFFFFFFF, 0xFF000000, 0xFFFFFFFF, 0xFF000000,
-        0xFF000000, 0xFFFFFFFF, 0xFF000000, 0xFFFFFFFF,
-    });
+    desc.data.subimage[0][0] = sg.asRange(img_data[0..size]);
     return .{ .desc = desc };
 }
