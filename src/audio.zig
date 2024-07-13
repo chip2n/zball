776c6ab0d5b0bcaf12a0af5d719d40d97b51c907
chip2n <andreas@arvidsson.io>
2024-07-13 09:18:13 +0200

    Clean up audio

--- src/audio.zig
+++ src/audio.zig
@@ -6,9 +6,8 @@ const saudio = sokol.audio;
 
 const g_volume = 0.3;
 const num_channels = 2;
-const sample_buf_length = 81920; // TODO
+const sample_buf_length = 4096; // TODO
 const sample_rate = 44100; // TODO
-// const samples_buf_length = 1000;
 
 const AudioHandle = usize;
 
@@ -55,76 +54,30 @@ pub const AudioSystem = struct {
 
     pub fn update(sys: *AudioSystem, time: f64) void {
         const num_frames: i32 = @intFromFloat(@ceil(sample_rate * (time - sys.time)));
-        // const num_samples: i32 = num_frames * num_channels;
 
         sys.time = time;
-
-        const Sorter = struct {
-            fn lessThanFn(_: void, lhs: AudioTrack, rhs: AudioTrack) bool {
-                if (lhs.clip == null) return false;
-                if (rhs.clip == null) return true;
-                const ca = clips.get(lhs.clip.?);
-                const cb = clips.get(rhs.clip.?);
-                const samples_a = ca.samples();
-                const samples_b = cb.samples();
-                return samples_a.len - lhs.frame < samples_b.len - rhs.frame;
-            }
-        };
-        std.mem.sort(AudioTrack, &sys.playing, {}, Sorter.lessThanFn);
-
-        // Calculate how many clips are currently playing
-        var num_concurrent_clips: usize = 0;
-        for (sys.playing) |p| {
-            if (p.clip == null) continue;
-            num_concurrent_clips += 1;
-        }
-
-        // TODO: Sanity check - have we sorted clips correctly?
-        for (0..num_concurrent_clips) |i| {
-            const p = sys.playing[i];
-            std.debug.assert(p.clip != null);
-        }
 
         // Clear out sample buffer
         for (&sys.samples) |*s| s.* = 0.0;
 
-        var num_written: i32 = 0;
-        var dst: []f32 = &sys.samples;
-        for (0..num_concurrent_clips) |i| {
-            var num_written_temp: usize = 0;
-            for (i..num_concurrent_clips) |j| {
-                var p = &sys.playing[j];
-                if (p.clip == null) continue;
-                std.debug.assert(p.clip != null);
+        const dst: []f32 = &sys.samples;
 
-                const clip = clips.get(p.clip.?);
-                const volume = 1 / @as(f32, @floatFromInt(num_concurrent_clips - i));
-                const count = writeSamples(clip, p.frame, @as(usize, @intCast(num_frames - num_written)), dst, volume * p.volume);
+        for (&sys.playing) |*p| {
+            if (p.clip == null) continue;
+            const clip = clips.get(p.clip.?);
+            const count = writeSamples(clip, p.frame, @as(usize, @intCast(num_frames)), dst, p.volume);
+            p.frame += count;
 
-                // std.log.warn("clip {}: frame: {}, count: {}, vol: {}", .{ j, p.frame, count, volume });
-
-                p.frame += count;
-
-                if (p.frame >= clip.frameCount()) {
-                    if (p.loop) {
-                        // TODO I don't think this is a perfect loop - we end with just a few samples and not fill all the frames
-                        std.log.warn("looping clip", .{});
-                        p.frame = 0;
-                    } else {
-                        std.log.warn("done", .{});
-                        p.clip = null;
-                        p.frame = 0;
-                    }
+            if (p.frame >= clip.frameCount()) {
+                if (p.loop) {
+                    // TODO I don't think this is a perfect loop
+                    p.frame = 0;
+                } else {
+                    p.clip = null;
+                    p.frame = 0;
                 }
-                num_written_temp = @max(num_written_temp, count);
             }
-            num_written += @intCast(num_written_temp);
-            dst = sys.samples[@intCast(num_written_temp)..];
         }
-        // if (num_written > 0) {
-        //     std.log.warn("num: {}", .{num_written});
-        //     std.log.warn("----", .{});
-        // }
 
         if (num_frames > 0) {
             _ = saudio.push(&(sys.samples[0]), num_frames);
@@ -139,25 +92,17 @@ pub const AudioSystem = struct {
         volume: f32,
     ) usize {
         const clip_samples = clip.samples();
-        // std.debug.assert(std.math.mod(usize, clip_samples.len, clip.header.nbrChannels) catch unreachable == 0);
-
         const sample_offset = frame_offset * clip.header.nbrChannels;
-
-        const frames_left = @divExact(clip_samples.len, clip.header.nbrChannels) - frame_offset; // TODO this doesn't support 2 channels
+        const frames_left = @divExact(clip_samples.len, clip.header.nbrChannels) - frame_offset;
         const frames_to_write = @min(sample_buf_length, @min(frame_count, frames_left)); // TODO rename sample_buf_length?
-
-        //std.log.warn("frame_offset: {}, samples_to_write: {}, frame_count: {}", .{ frame_offset, samples_to_write, frame_count });
 
         const src = clip_samples[sample_offset .. sample_offset + frames_to_write * clip.header.nbrChannels];
         const dst = output[0 .. frames_to_write * num_channels];
-        // const dst = output[0..];
 
-        // TODO 2 channels - right now we're reading src as 1 channel
         for (src, 0..) |s, i| {
             const fs: f32 = @floatFromInt(s);
             const div: usize = if (fs < 0) @abs(std.math.minInt(i16)) else std.math.maxInt(i16);
             const result = (fs / @as(f32, @floatFromInt(div)));
-            // const result = (fs / @as(f32, @floatFromInt(std.math.maxInt(i16))));
             std.debug.assert(-1 <= result and result <= 1);
             if (clip.header.nbrChannels == 1) {
                 dst[i * num_channels + 0] += result * volume * g_volume;
