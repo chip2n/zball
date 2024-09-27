dcea58f1e5a88ed3f787076f7fccd9175058f617
chip2n <andreas@arvidsson.io>
2024-09-27 06:42:15 +0200

    Fix shell.html

--- build.zig
+++ build.zig
@@ -313,7 +313,7 @@ fn buildWeb(
         .use_emmalloc = true,
         .use_filesystem = false,
         .extra_args = &.{"-sUSE_OFFSET_CONVERTER=1"},
-        .shell_file_path = b.path("src/minshell.html"),
+        .shell_file_path = b.path("src/shell.html"),
     });
 
     const run = sokol.emRunStep(b, .{ .name = "game", .emsdk = emsdk });
