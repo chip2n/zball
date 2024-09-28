9d80264a722cd9a25c4736ae2c42dc827971b041
chip2n <andreas@arvidsson.io>
2024-09-28 18:12:10 +0200

    Update flake

--- flake.nix
+++ flake.nix
@@ -1,31 +1,29 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
     zig_overlay.url = "github:mitchellh/zig-overlay";
   };
 
-  outputs = { self, nixpkgs, zig_overlay, ... }: let
-    system = "x86_64-linux";
-    zig = zig_overlay.packages.${system}.master-2024-08-03;
-  in {
-    devShells.${system}.default = let
-      pkgs = import nixpkgs {
-        inherit system;
+  outputs = { self, nixpkgs, flake-utils, zig_overlay, ...}:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs { inherit system; };
+      zig = zig_overlay.packages.${system}.master-2024-08-03;
+    in {
+      devShell = pkgs.mkShell {
+        packages = [
+          zig
+          pkgs.alsa-lib
+          pkgs.alsa-plugins
+          pkgs.glfw
+          pkgs.xorg.libX11
+          pkgs.xorg.libXi
+          pkgs.xorg.libXcursor
+        ];
+        shellHook = ''
+          export ALSA_PLUGIN_DIR=${pkgs.alsa-plugins}/lib/alsa-lib
+          echo "zig $(zig version)"
+        '';
       };
-    in pkgs.mkShell {
-      packages = [
-        zig
-        pkgs.alsa-lib
-        pkgs.alsa-plugins
-        pkgs.glfw
-        pkgs.xorg.libX11
-        pkgs.xorg.libXi
-        pkgs.xorg.libXcursor
-      ];
-      shellHook = ''
-        export ALSA_PLUGIN_DIR=${pkgs.alsa-plugins}/lib/alsa-lib
-        echo "zig $(zig version)"
-      '';
-    };
-  };
+    });
 }
